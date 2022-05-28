61d52eb61443ae9e67a22a6118e4ccedbe6d4a0c
Moonflakes <33527879+Moonflakes@users.noreply.github.com>
2022-05-28 17:07:42 +0200

    init to without data

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -12,8 +12,8 @@ class Grid extends React.Component {
     super(props);
 
     this.state = {
-      data: this.props.data,
-      selectedData: "data",
+      data: {},
+      selectedData: "",
     };
   }
 
@@ -54,14 +54,13 @@ class Grid extends React.Component {
   render() {
     const { data } = this.state;
     const hoursOfDay = getHoursOfDay(data.startTime, data.endTime);
+    const day = data.day ? this.dateToDdMmYyyy(data.day) : "...";
 
     return (
       <div className="Grid">
-        <div className="Title">
-          {"Grille du " + this.dateToDdMmYyyy(data.day)}
-        </div>
+        <div className="Title">{"Grille du " + day}</div>
         <fieldset style={{ marginBottom: "20px" }}>
-          <legend>Select data:</legend>
+          <legend>Sélectionner le programme :</legend>
           <div
             onChange={(event) => {
               this.setState({
@@ -71,71 +70,85 @@ class Grid extends React.Component {
             }}
           >
             <div>
-              <input type="radio" value="data" name="data" /> Filled data
+              <input type="radio" value="data" name="data" /> Programme rempli
             </div>
             <div>
               <input type="radio" value="dataWithNoShowSlots" name="data" />{" "}
-              Data with no show slots
+              Programme incomplet
             </div>
           </div>
         </fieldset>
 
-        <div className="Chns">
-          <div style={{ marginTop: "18px" }}>
-            {hoursOfDay.map((hour, index) => {
+        {!data.day ? (
+          <>
+            <h1>Aucune données</h1>
+            <span>Sélectionner le programme à visualiser</span>
+          </>
+        ) : (
+          <div className="Chns">
+            <div style={{ marginTop: "18px" }}>
+              {hoursOfDay?.map((hour, index) => {
+                return (
+                  <TimeSlot
+                    show={hour}
+                    key={`hour-${index}`}
+                    showHours={false}
+                  />
+                );
+              })}
+            </div>
+            {data.chns.map((chn) => {
+              const hasProgram = chn.shows.length > 0;
+              const withoutProgram = {
+                title: "Aucun programme",
+                startTime: data.startTime,
+                endTime: data.endTime,
+              };
               return (
-                <TimeSlot show={hour} key={`hour-${index}`} showHours={false} />
+                <div
+                  key={chn.key}
+                  style={{
+                    textAlign: "center",
+                    position: "relative",
+                    width: `${100 / data.chns.length}%`,
+                  }}
+                >
+                  <div className="Chn">{chn.key}</div>
+                  {!hasProgram && <TimeSlot show={withoutProgram} showHours />}
+                  {chn?.shows.map((show, index) => {
+                    const { startTime, endTime } = data;
+                    const withoutTimeSlot = this.completeWithoutShowTime(
+                      startTime,
+                      endTime,
+                      show,
+                      chn.shows[index - 1]
+                    );
+                    const withoutLastTimeSlot = chn.shows.length - 1 ===
+                      index &&
+                      show.endTime < endTime && {
+                        title: "Sans programme",
+                        startTime: show.endTime,
+                        endTime: endTime,
+                      };
+                    return (
+                      <Fragment
+                        key={`show-${index}-${this.state.selectedData}`}
+                      >
+                        {withoutTimeSlot && (
+                          <TimeSlot show={withoutTimeSlot} showHours />
+                        )}
+                        <TimeSlot show={show} showHours />
+                        {withoutLastTimeSlot && (
+                          <TimeSlot show={withoutLastTimeSlot} showHours />
+                        )}
+                      </Fragment>
+                    );
+                  }, chn.shows)}
+                </div>
               );
             })}
           </div>
-          {data.chns.map((chn) => {
-            const hasProgram = chn.shows.length > 0;
-            const withoutProgram = {
-              title: "Aucun programme",
-              startTime: data.startTime,
-              endTime: data.endTime,
-            };
-            return (
-              <div
-                key={chn.key}
-                style={{
-                  textAlign: "center",
-                  position: "relative",
-                  width: `${100 / data.chns.length}%`,
-                }}
-              >
-                <div className="Chn">{chn.key}</div>
-                {!hasProgram && <TimeSlot show={withoutProgram} showHours />}
-                {chn.shows.map((show, index) => {
-                  const { startTime, endTime } = data;
-                  const withoutTimeSlot = this.completeWithoutShowTime(
-                    startTime,
-                    endTime,
-                    show,
-                    chn.shows[index - 1]
-                  );
-                  const withoutLastTimeSlot = chn.shows.length - 1 === index &&
-                    show.endTime < endTime && {
-                      title: "Sans programme",
-                      startTime: show.endTime,
-                      endTime: endTime,
-                    };
-                  return (
-                    <Fragment key={`show-${index}-${this.state.selectedData}`}>
-                      {withoutTimeSlot && (
-                        <TimeSlot show={withoutTimeSlot} showHours />
-                      )}
-                      <TimeSlot show={show} showHours />
-                      {withoutLastTimeSlot && (
-                        <TimeSlot show={withoutLastTimeSlot} showHours />
-                      )}
-                    </Fragment>
-                  );
-                }, chn.shows)}
-              </div>
-            );
-          })}
-        </div>
+        )}
       </div>
     );
   }
